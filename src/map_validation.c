33262f70924ee3f2478f86b4e76773fdc9b6efe8
s98ade <sofia.ade@gmx.de>
2024-07-03 13:30:13 +0300

    fixed map validation

--- src/map_validation.c
+++ src/map_validation.c
@@ -6,7 +6,7 @@
 /*   By: sofia <sofia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 12:04:39 by sade              #+#    #+#             */
-/*   Updated: 2024/07/03 12:58:54 by sofia            ###   ########.fr       */
+/*   Updated: 2024/07/03 13:28:57 by sofia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ void validate_map_borders(t_map *data)
             map_error(5, NULL);
         i++;
     }
-    i = (data->map_width * data->map_height) - data->map_width;
-    while(i < (data->map_width * data->map_height))
+    i = 0;
+    while(i < data->map_width)
     {
-        if(data->map[0][i] != '1')
+        if(data->map[data->map_height - 1][i] != '1')
             map_error(5, NULL);
         i++;
     }
@@ -35,17 +35,13 @@ void validate_map_borders(t_map *data)
 void validate_map_sides(t_map *data)
 {
     int i;
-    int start;
-    int end;
 
     i = 0;
     while(i < data->map_height)
     {
-        start = i * data->map_width;
-        end = start + data->map_width - 1;
-        if(data->map[start][0] != '1')
+        if(data->map[i][0] != '1')
             map_error(5, NULL);
-        if(data->map[end][0] != '1')
+        if(data->map[i][data->map_width - 1] != '1')
             map_error(5, NULL);
         i++;
     }
@@ -74,11 +70,15 @@ void check_items(t_map *data)
         j++;
     }
     if(data->count->c == 0 || data->count->p == 0 || data->count->e == 0 || data->count->p > 1)
-        map_error(4, NULL);
+        map_error(6, NULL);
 }
 
 void validate_map(t_map *data)
 {
+    data->count->c = 0;
+    data->count->e = 0;
+    data->count->p = 0;
+    
     validate_map_borders(data);
     validate_map_sides(data);
     check_items(data);  
